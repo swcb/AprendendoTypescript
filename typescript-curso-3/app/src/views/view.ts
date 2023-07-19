0a75202dc12915820ff99ab1f1ad95e6ccc6a158
Willian Caires <willianaika12@gmail.com>
2023-07-19 00:29:40 -0300

    Usando decorators Usado o decorators para medir o tempo de execução de adicionar e update

--- typescript-curso-3/app/src/views/view.ts
+++ typescript-curso-3/app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecução } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -15,6 +17,7 @@ export abstract class View<T> {
         }
     }
 
+    @logarTempoDeExecução()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
