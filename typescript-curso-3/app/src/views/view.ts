af107da1d64fef187efe8aa23264ed1731c0123c
Willian Caires <willianaika12@gmail.com>
2023-07-23 10:56:53 -0300

    Criando decorator inspect

--- typescript-curso-3/app/src/views/view.ts
+++ typescript-curso-3/app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecução } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
@@ -17,7 +18,8 @@ export abstract class View<T> {
         }
     }
 
-    @logarTempoDeExecução()
+    @logarTempoDeExecução(true)
+    @inspect
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
