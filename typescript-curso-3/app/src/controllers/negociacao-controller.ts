75873736246836920dcc5841fd189bfd77f06f4b
Willian Caires <willianaika12@gmail.com>
2023-07-23 18:20:21 -0300

    Adicionando camada de serviço Encapsuçando o consumo da API em uma classe de servico

--- typescript-curso-3/app/src/controllers/negociacao-controller.ts
+++ typescript-curso-3/app/src/controllers/negociacao-controller.ts
@@ -6,7 +6,7 @@ import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
-import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js';
+import { NegociacoesService } from '../services/negociacoes-service.js';
 
 export class NegociacaoController {
     @domInjector('#data')
@@ -18,6 +18,7 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
+    private negociacoesService = new NegociacoesService();
 
     constructor() {
         this.negociacoesView.update(this.negociacoes);
@@ -45,17 +46,8 @@ export class NegociacaoController {
     }
 
     public importaDados(): void {
-        fetch('http://localhost:8080/dados')
-            .then(res => { return res.json() })
-            .then((dados: NegociacoesDoDia[]) => {
-                return dados.map(dadoDeHoje => {
-                    return new Negociacao(
-                        new Date(),
-                        dadoDeHoje.vezes,
-                        dadoDeHoje.montante
-                    )
-                });
-            })
+        this.negociacoesService
+            .obterNegociacoesDoDia()
             .then(negociacoesDeHoje => {
                 for(let negociacao of negociacoesDeHoje){
                     this.negociacoes.adiciona(negociacao);
