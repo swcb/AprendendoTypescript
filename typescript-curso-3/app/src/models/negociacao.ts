0693ab3116fc4c225183bb4dfa23e7dc85e50444
Willian Caires <willianaika12@gmail.com>
2023-07-23 19:19:18 -0300

    Classes Comparaveis usando Interfaces e Generecs

--- typescript-curso-3/app/src/models/negociacao.ts
+++ typescript-curso-3/app/src/models/negociacao.ts
@@ -1,6 +1,7 @@
+import { Comparavel } from "../interfaces/comparavel.js";
 import { Imprimivel } from "../utils/imprimivel.js"
 
-export class Negociacao implements Imprimivel {
+export class Negociacao implements Imprimivel, Comparavel<Negociacao> {
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
@@ -30,5 +31,11 @@ export class Negociacao implements Imprimivel {
             Quantidade: ${this.quantidade},
             Valor: ${this.valor}
         `;
+    }
+
+    public ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() === negociacao.data.getDate()
+        && this.data.getMonth() === negociacao.data.getMonth()
+        && this.data.getFullYear() === negociacao.data.getFullYear();
     }
 }
